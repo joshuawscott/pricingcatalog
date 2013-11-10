330c335c9e7be9bdd2d5e2a3bf5030eed5ccad70
Joshua Scott <joshua.scott@gmail.com>
2013-11-09 20:30:29 -0600

    Fix symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,6 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
-  after :deploy, :symlink_config
+  after "deploy", "deploy:symlink_config"
 end
 
