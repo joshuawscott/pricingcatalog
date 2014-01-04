f3f13361c3c23d7bf7ddfe703afe7e1ef5695536
Joshua Scott <joshua.scott@gmail.com>
2014-01-03 23:51:17 -0600

    add user edit

---
+++ app/controllers/admin_users_controller.rb
@@ -0,0 +1,73 @@
+class AdminUsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /admin_users
+  # GET /admin_users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /admin_users/1
+  # GET /admin_users/1.json
+  def show
+  end
+
+  # GET /admin_users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /admin_users/1/edit
+  def edit
+  end
+
+  # POST /admin_users
+  # POST /admin_users.json
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to admin_user_url(@user), notice: 'User was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /admin_users/1
+  # PATCH/PUT /admin_users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to admin_user_url(@user), notice: 'Admin user was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /admin_users/1
+  # DELETE /admin_users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Pricingcatalog::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users/:id' => 'devise/registrations#update', :as => 'registration'
+  end
+
+  resources :admin_users
+
   resources :costs
 
   resources :list_prices
